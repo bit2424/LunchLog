0f0054b45ed175e418f45fac672e0d0b33e912c0
bit2424 <nelsonquinones2424@gmail.com>
2025-09-20 10:10:28 +0200

    :sparkles: Extend user authentication endpoints with JWT.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
+    # Session-based authentication
     path('auth/signup/', views.CreateUserView.as_view(), name='signup'),
     path('auth/login/', views.LoginView.as_view(), name='login'),
+    
+    # JWT authentication
+    path('auth/jwt/create/', TokenObtainPairView.as_view(), name='jwt_create'),
+    path('auth/jwt/refresh/', TokenRefreshView.as_view(), name='jwt_refresh'),
+    path('auth/jwt/verify/', TokenVerifyView.as_view(), name='jwt_verify'),
+    
+    # User profile
+    path('me/', views.CurrentUserView.as_view(), name='current_user'),
 ]
